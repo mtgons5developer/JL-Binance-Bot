f202f5b911f1d5c11edced243c539efd1ebb6f86
Ernesto Almeda <mtgons5.ea@gmail.com>
2022-05-15 04:56:55 +0800

    MACD working

--- MACD.py
+++ MACD.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 
 import os
 from binance.client import AsyncClient
+import talib
 import asyncio
 import pprint
 import pandas as pd
@@ -19,13 +20,54 @@ class PatternDetect:
         client = await AsyncClient.create(config.BINANCE_API_KEY,config.BINANCE_SECRET_KEY)
         msg = await client.futures_historical_klines(symbol=symbol, interval="15m", start_str="2022-14-05 03:00:00", end_str=None)
         data = self.get_data_frame(symbol=symbol, msg=msg) 
-        shortEMA = df['close'].ewm(span=12, adjust=False).mean()
-        longEMA = df['close'].ewm(span=26, adjust=False).mean()
-        MACD = shortEMA - longEMA 
-        print(MACD)
-
+        # macd, macdsignal, macdhist = talib.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+        # print(macd)
+        # print(macdsignal)
+        # print(macdhist)
+        # quit()
+        # self.shortEMA()
+        # self.longEMA()
+        self.MACD()
+        # self.plot_graph(df)        
+        # shortEMA = df['Close'].ewm(span=12, adjust=False).mean()
+        # longEMA = df['Close'].ewm(span=26, adjust=False).mean()
+        # MACD = shortEMA - longEMA 
+        # signal = MACD.ewm(span=9, adjust=False).mean()
+        # df['MACD'] = MACD 
+        # df['signal'] = signal
+        # df['Trigger'] = np.where(df['MACD'] > df['signal'], 1, 0)
+        # df['Position'] = df['Trigger'].diff()
+        # print(df)
         await client.close_connection()
 
+    def plot_graph(df):
+        df=df.astype(float)
+        df[['Close', 'MACD', 'signal']].plot()
+        plt.xlabel('Time', fontsize=18)
+        plt.ylabel('Close Price', fontsize=18)
+        x_axis = df.index
+        plt.show()
+
+    def MACD(self):
+        shortEMA = df['Close'].ewm(span=12, adjust=False).mean()
+        longEMA = df['Close'].ewm(span=26, adjust=False).mean()
+        MACD = shortEMA - longEMA 
+        signal = MACD.ewm(span=9, adjust=False).mean()
+        df['MACD'] = MACD 
+        df['signal'] = signal
+        df['Trigger'] = np.where(df['MACD'] > df['signal'], 1, 0)
+        df['Position'] = df['Trigger'].diff()
+        # print(df)
+        df['Buy'] = np.where(df['Position'] == 1, df['Close'], 'NaN')
+        df['Sell'] = np.where(df['Position'] == -1, df['Close'], 'NaN')
+
+        with open('output.txt', 'w') as f:
+            f.write(
+                df.to_string()
+            )
+        
+
+        # self.plot_graph(df)
 
     def get_data_frame(self, symbol, msg):
         global df
@@ -46,8 +88,8 @@ class PatternDetect:
         cc = df["Close"]
         close = cc[rows_count - 1]
         hh = df["High"]
-        high = hh[rows_count - 1]   
-        print(df)
+        high = hh[rows_count - 1]  
+        # print(df)
 
         return df
 

bced73fafc0d011fb251d6972d6d7fa37f5a8283
Ernesto Almeda <mtgons5.ea@gmail.com>
2022-05-15 10:11:02 +0800

    MACD+RSI+StochRSI+

--- MACD.py
+++ MACD.py
@@ -18,13 +18,49 @@ class PatternDetect:
     async def main(self):
 
         client = await AsyncClient.create(config.BINANCE_API_KEY,config.BINANCE_SECRET_KEY)
-        msg = await client.futures_historical_klines(symbol=symbol, interval="15m", start_str="2022-14-05 03:00:00", end_str=None)
+        msg = await client.futures_historical_klines(symbol=symbol, interval="1h", start_str="2022-13-05 00:00:00", end_str=None)
         data = self.get_data_frame(symbol=symbol, msg=msg) 
-        # macd, macdsignal, macdhist = talib.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
-        # print(macd)
-        # print(macdsignal)
-        # print(macdhist)
-        # quit()
+
+        RSI = talib.RSI(df['Close'], timeperiod=14)
+        BOP = talib.BOP(df['Open'], df['High'], df['Low'], df['Close'])
+        macd, macdsignal, macdhist = talib.MACD(df['Close'], fastperiod=3, slowperiod=10, signalperiod=16) #HIGH TF
+        fastk, fastd = talib.STOCHRSI(df['Close'], timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0)
+        # Find Entry Trigger
+        # Open     High      Low      Close  BOP   RSI   fastk  fastd   MACD  Signal  History
+        # 29482.5  29495.0  29241.9  29313.5 -0.7  44.0    0.0   55.0 -100.0  -100.0     -0.0
+        # 29313.6  29365.2  28812.4  29065.0 -0.4  40.0    0.0   22.0 -188.0  -110.0    -77.0   
+        # 29065.3  29249.9  28929.0  29091.4  0.1  40.0    8.0    3.0 -198.0  -121.0    -78.0
+        # 29091.4  29159.1  28752.0  28827.2 -0.6  36.0    0.0    3.0 -268.0  -138.0   -130.0 =======
+        # Trigger Point
+        # 28827.9  28976.9  28644.0  28930.1  0.3  39.0   34.0   14.0 -240.0  -150.0    -90.0 =======
+        # 28930.1  29087.7  28751.2  28812.6 -0.3  37.0   19.0   18.0 -244.0  -161.0    -83.0 
+
+        # Open TF 1 < Close TF 4 == Trigger
+        # MACD =    "4x TF going lower or equal or TF 1 is < TF2-4" Then Long
+        # BOP =     "4x TF going lower or equal or TF 1 is < TF2-4" Then Long
+        # RSI =     "4x TF going lower or equal or TF 1 is < TF2-4" Then Long
+        # fastd =   "4x TF going lower or equal or TF 1 is < TF2-4" Then Long
+        # fastk =   "4x TF going lower or equal or TF 1 is > TF2-4" Then Long
+        # History = "4x TF going lower or equal or TF 1 is < TF2-4" Then Long
+
+        df['BOP'] = round(BOP, 1)
+        df['RSI'] = round(RSI)
+        df['fastk'] = round(fastk)
+        df['fastd'] = round(fastd)
+        df['MACD'] = round(macd )
+        df['Signal'] = round(macdsignal)
+        df['History'] = round(macdhist)
+
+        df['Trigger'] = np.where(df['MACD'] > df['Signal'], 1, 0)
+        df['Position'] = df['Trigger'].diff()
+        df['Buy'] = np.where(df['Position'] == 1, df['Close'], 'NaN')
+        df['Sell'] = np.where(df['Position'] == -1, df['Close'], 'NaN')                     
+        print(df)
+        with open('output.txt', 'w') as f:
+            f.write(
+                df.to_string()
+            )        
+        quit()
         # self.shortEMA()
         # self.longEMA()
         self.MACD()
@@ -57,9 +93,9 @@ class PatternDetect:
         df['signal'] = signal
         df['Trigger'] = np.where(df['MACD'] > df['signal'], 1, 0)
         df['Position'] = df['Trigger'].diff()
-        # print(df)
         df['Buy'] = np.where(df['Position'] == 1, df['Close'], 'NaN')
         df['Sell'] = np.where(df['Position'] == -1, df['Close'], 'NaN')
+        print(df)
 
         with open('output.txt', 'w') as f:
             f.write(
@@ -74,17 +110,17 @@ class PatternDetect:
 
         df = pd.DataFrame(msg)
         df.columns = ['Time','Open', 'High', 'Low', 'Close', 'Volume','CloseTime', 'qav','num_trades','taker_base_vol', 'taker_quote_vol', 'ignore']
-        df = df.loc[:, ['Time','Open', 'High', 'Low', 'Close', 'Volume']]
-        df["Time"] = pd.to_datetime(df["Time"], unit='ms')
+        df = df.loc[:, ['Open', 'High', 'Low', 'Close']]
+        # df["Time"] = pd.to_datetime(df["Time"], unit='ms')
         df["Open"] = df["Open"].astype(float)
         df["High"] = df["High"].astype(float)
         df["Low"] = df["Low"].astype(float)
         df["Close"] = df["Close"].astype(float)
-        df["Volume"] = df["Volume"].astype(float)
+        # df["Volume"] = df["Volume"].astype(float)
 
         rows_count = len(df.index)
-        vv = df["Volume"]
-        volume = round(vv[rows_count - 1])
+        # vv = df["Volume"]
+        # volume = round(vv[rows_count - 1])
         cc = df["Close"]
         close = cc[rows_count - 1]
         hh = df["High"]

0f6229212fd44365eb90af9fc973f05fd0d4df7b
Ernesto Almeda <mtgons5.ea@gmail.com>
2022-05-18 15:55:48 +0800

    Final v1

--- BTCUSDT-JL.py
+++ BTCUSDT-JL.py
@@ -139,118 +139,129 @@ class PatternDetect:
         fastk, fastd = talib.STOCHRSI(df['Close'], timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0)
 
         df['BOP'] = round(BOP, 2)
-        # df['RSI'] = round(RSI)
+        df['RSI'] = round(RSI)
         df['fastd'] = round(fastd)
         df['fastk'] = round(fastk)
 
-        # df['MACD'] = round(macd )
-        # df['Signal'] = round(macdsignal)
-        # df['History'] = round(macdhist)                 
         rr = len(df.index)
 
         pp = df.tail(3)
+
         dd = pd.DataFrame(pp)
-        dd = dd.loc[:, ['Time', 'BOP', 'fastd', 'fastk']]
-        # dd = dd.loc[:, ['Time', 'BOP', 'RSI', 'fastd', 'MACD', 'Signal', 'History']]
-        BOPS = np.where(df["BOP"][rr - 3] > df['BOP'][rr -2], -1, 1)
-        # RSIS = np.where(df["RSI"][rr - 3] > df['RSI'][rr -2], -1, 1)
-        fastdS = np.where(df["fastd"][rr - 3] > df['fastd'][rr -2], -1, 1)
-        fastkS = np.where(df["fastk"][rr - 3] > df['fastk'][rr -2], -1, 1)
-        # MACDS = np.where(df["MACD"][rr - 3] > df['MACD'][rr -2], -1, 1)
-        # SignalS = np.where(df["Signal"][rr - 3] > df['Signal'][rr -2], -1, 1)
-        # HistoryS = np.where(df["History"][rr - 3] > df['History'][rr -2], -1, 1)                                
-
-        dd['BOPT'] = BOPS
-        # dd['RSIT'] = RSIS
-        dd['fastdT'] = fastdS
-        dd['fastkT'] = fastkS
-        # dd['MACDT'] = MACDS
-        # dd['SignalT'] = SignalS
-        # dd['HistoryT'] = HistoryS
+        dd = dd.loc[:, ['Time', 'Open', 'High', 'Low', 'Close', 'BOP', 'RSI', 'fastd', 'fastk']]
         
-        Position = BOPS + fastdS
+        BOP2 = dd["BOP"][rr - 2]
+        BOP3 = dd["BOP"][rr - 3]
 
-        signal1 = 0
-        signal2 = 0
+        RSI2 = dd["RSI"][rr - 2]
+        RSI3 = dd["RSI"][rr - 3]
 
-        if Position == 2: 
-            signal1 = 1
-        elif Position < 0:
-            signal1 = -1
-        else: 
-            signal1 = 0
+        fastd2 = dd["fastd"][rr - 2]
+        fastd3 = dd["fastd"][rr - 3]
 
-        BOP2 = float(df['BOP'][rr - 2])
+        fastk2 = dd["fastk"][rr - 2]
+        fastk3 = dd["fastk"][rr - 3]
 
-        if BOP2 < 0.5 and BOP2 >= 0: 
-            signal2 = 1
-        elif BOP2 < -0.6 and BOP2 >= -0.9:
-            signal2 = 1
-        elif BOP2 > 0.5 and BOP2 <= 0.9: 
-            signal2 = -1
-        elif BOP2 > -0.6 and BOP2 >= -0.1:
-            signal2 = -1
-        else:
-            signal2 = 0
+        # print(dd)
 
-        signalT = signal1 + signal2
+        # 26  2022-05-11 10:00:00  31742.2  32197.6  31555.0  31835.0  0.14  55.0  100.0  100.0
+        # 27  2022-05-11 11:00:00  31835.1  31895.8  31452.0  31517.1 -0.72  51.0   91.0   73.0
 
-        if signalT == 2:
-            side = "BUY"
-        elif signalT < 0:
+        BOP3 = 0.14  
+        RSI3 = 55.0   
+        fastd3 = 100.0   
+        fastk3 = 100.0
+        BOP2 = -0.72
+        RSI2 = 51.0   
+        fastd2 = 91.0  
+        fastk2 = 73
+
+                        #   Time     Open     High      Low    Close   BOP   RSI  fastd  fastk
+        # 67 2022-05-17 05:00:00  30365.1  30519.3  30260.2  30445.6  0.31  58.0  100.0  100.0
+        # 68 2022-05-17 06:00:00  30445.6  30539.0  30335.0  30514.5  0.34  59.0  100.0  100.0
+        # 69 2022-05-17 07:00:00  30514.5  30576.4  30335.0  30394.3 -0.50  56.0   67.0    0.0 SHORT
+
+        if BOP2 > BOP3 and RSI2 == RSI3 and RSI2 > 55 and fastd2 == fastd3 and fastd2 == 100 and fastk2 == fastk3 and fastk2 == 100:
             side = "SELL"
+            print("1")
+        elif BOP2 > BOP3 and RSI2 > RSI3 and RSI2 > 55 and fastd2 == fastd3 and fastd2 == 100 and fastk2 == fastk3 and fastk2 == 100:
+            side = "SELL"   
+            print("2")
+                        #   Time     Open     High      Low    Close   BOP   RSI  fastd  fastk            
+        # 70 2022-05-17 08:00:00  30394.2  30794.0  30375.0  30559.9  0.40  59.0   67.0  100.0
+        # 71 2022-05-17 09:00:00  30560.0  30717.3  30550.0  30625.0  0.39  60.0   67.0  100.0
+        # 72 2022-05-17 10:00:00  30625.0  30641.6  30429.7  30440.0 -0.87  56.0   67.0    0.0 SHORT
+        #                  
+        elif BOP2 < BOP3 and RSI2 == RSI3 and RSI2 > 55 and fastd2 == fastd3 and fastd2 > 60 and fastk2 == fastk3 and fastk2 == 100:
+            side = "SELL"   
+            print("3")     
+        elif BOP2 < BOP3 and RSI2 > RSI3 and RSI2 > 55 and fastd2 == fastd3 and fastd2 > 60 and fastk2 == fastk3 and fastk2 == 100:
+            side = "SELL"        
+            print("4")
+                        #   Time     Open     High      Low    Close   BOP   RSI  fastd  fastk            
+        # 42 2022-05-16 04:00:00  30314.9  30436.9  30105.0  30313.9 -0.00  50.0    0.0    0.0
+        # 43 2022-05-16 05:00:00  30313.4  30461.3  30233.0  30280.0 -0.15  49.0    0.0    0.0 
+        # 44 2022-05-16 06:00:00  30279.9  30280.0  29361.1  29557.4 -0.79  38.0    0.0    0.0 SHORT
+
+        elif BOP2 < BOP3 and RSI2 == RSI3 and RSI2 < 50 and fastd2 == fastd3 and fastd2 < 10 and fastk2 == fastk3 and fastk2 < 10:
+            side = "SELL" 
+            print("5")       
+        elif BOP2 < BOP3 and RSI2 < RSI3 and RSI2 < 50 and fastd2 == fastd3 and fastd2 < 10 and fastk2 == fastk3 and fastk2 < 10:
+            side = "SELL"   
+            print("6")
+                        #   Time     Open     High      Low    Close   BOP   RSI  fastd  fastk            
+        # 38 2022-05-15 23:00:00  31087.7  31422.6  31035.9  31324.4  0.61  72.0  100.0  100.0
+        # 39 2022-05-16 00:00:00  31324.3  31327.4  31040.0  31105.0 -0.76  66.0   67.0    0.0
+        # 40 2022-05-16 01:00:00  31105.0  31152.0  30678.1  30768.6 -0.71  58.0   33.0    0.0 SHORT
+
+        elif BOP2 < 0 and BOP2 < BOP3 and RSI2 < RSI3 and RSI2 < 70 and fastd2 < fastd3 and fastk2 < fastk3 and fastd3 == 100 and fastk2 == 100:
+            side = "SELL"
+            print("7-1")
+
+                        #   Time     Open     High      Low    Close   BOP   RSI  fastd  fastk
+        # 26  2022-05-11 10:00:00  31742.2  32197.6  31555.0  31835.0  0.14  55.0  100.0  100.0
+        # 27  2022-05-11 11:00:00  31835.1  31895.8  31452.0  31517.1 -0.72  51.0   91.0   73.0
+        # 28  2022-05-11 12:00:00  31517.1  31776.0  29000.0  29298.7 -0.80  34.0   58.0    0.0 SHORT =======
+        elif BOP3 > 0 and BOP2 < 0 and BOP2 < BOP3 and RSI2 < RSI3 and RSI2 < 55 and fastd2 < fastd3 and fastk2 < fastk3 and fastd3 == 100 and fastk3 == 100:
+            side = "SELL"
+            print("7-2")        
+                        #   Time     Open     High      Low    Close   BOP   RSI  fastd  fastk            
+        # 89 2022-05-18 03:00:00  30204.8  30205.4  29700.0  29815.2 -0.77  42.0    8.0    0.0
+        # 90 2022-05-18 04:00:00  29815.3  29913.0  29740.0  29809.5 -0.03  42.0    8.0    0.0
+        # 91 2022-05-18 05:00:00  29809.5  30021.0  29779.7  30002.2  0.80  47.0   20.0   61.0 LONG  
+ 
+        elif BOP2 > BOP3 and RSI2 == RSI3 and RSI2 < 50 and fastd2 == fastd3 and fastd2 < 10 and fastk2 == fastk3 and fastk2 < 10:
+            side = "BUY"
+            print("8")
+                        #   Time     Open     High      Low    Close   BOP   RSI  fastd  fastk
+        # 44 2022-05-16 08:00:00  29572.7  29900.0  29554.4  29782.0  0.61  43.0   14.0   38.0
+        # 45 2022-05-16 09:00:00  29782.0  29782.1  29449.3  29595.7 -0.56  40.0   19.0   17.0
+
+        # 46 2022-05-16 10:00:00  29595.7  30196.1  29417.3  30161.8  0.73  51.0   52.0  100.0 LONG
+        elif BOP2 < BOP3 and RSI2 < RSI3 and RSI2 < 45 and fastd2 > fastd3 and fastk2 < fastk3 and fastd2 < 20 and fastk2 < 20:
+            side = "BUY"
+            print("9")
+                        #   Time     Open     High      Low    Close   BOP   RSI  fastd  fastk
+        # 27  2022-05-11 11:00:00  31835.1  31895.8  31452.0  31517.1 -0.72  51.0   91.0   73.0
+        # 28  2022-05-11 12:00:00  31517.1  31776.0  29000.0  29298.7 -0.80  34.0   58.0    0.0 
+        # 29  2022-05-11 13:00:00  29294.6  31889.6  29118.2  31223.0  0.70  50.0   50.0   76.0 LONG =======
+        elif BOP3 < 0 and BOP2 < -0.7 and BOP3 < -0.7 and BOP2 < BOP3 and RSI2 < RSI3 and RSI2 < 35 and fastd2 < fastd3 and fastk2 < fastk3 and fastd3 <= 100 and fastk2 == 0:
+            side = "BUY"
+            print("10")
+
         else:
             side = "NONE"
 
-        dd['Position'] = side
-
-        print(dd)
-        print(signal1)
-        print(signal2)
-        print(signalT)
+        if side == "BUY" or side == "SELL":
+            print(dd)
+            print(side)
+        else:
+            print(side) 
 
         with open('output.txt', 'w') as f:
             f.write(
                 dd.to_string()
-            )  
-
-    def d_Candle(self):
-        global open
-
-        cc = df["Open"]
-        rw = len(df.index)
-        tt = str(cc[rw - 1])
-        open = float(tt)
-        # print(open)
-
-    def d_RSI(self):
-        global rsi, error_set
-
-        rsi = talib.RSI(df["Close"])
-        rw = len(df.index)
-        tt = str(rsi[rw - 1])
-        rsi = round(float(tt))
-
-        if tt == "nan":
-            print("\n RSI: ERROR deltatime adjust to a higher value\n")
-            # db.put_dateErrorRSI(timeframe, pair)
-            error_set = 1
-
-#=====================================================================================================================
-
-    def d_SMA(self):
-        global curPrice, sma, error_set
-
-        sma = talib.SMA(df['Close'])
-        rw = len(df.index)
-        tt = str(sma[rw - 1])
-        sma = round(float(tt), 6)
-        curPrice = df['Close'].iloc[-1]
-        
-        if tt == "nan":
-            print("\n SMA: ERROR deltatime adjust to a higher value\n")
-            # db.put_dateErrorSMA(timeframe, pair)
-            error_set = 1   
+            )
             
 #=====================================================================================================================
 

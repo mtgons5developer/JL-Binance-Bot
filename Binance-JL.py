4182dc9587172f4ed606b08c1c9e29c66ea4e45b
Ernesto Almeda <mtgons5.ea@gmail.com>
2022-05-27 20:39:21 +0800

    JL

--- Binance-JL.py
+++ Binance-JL.py
@@ -1,141 +1,89 @@
 from datetime import datetime
+from datetime import timedelta
 
+import schedule
 import time
 import subprocess
+import schedule
 
 from binance.client import Client
 
+import callDB
+db = callDB.call()
+
 import config
 
-def main():
+# https://stackoverflow.com/questions/22715086/scheduling-python-script-to-run-every-hour-accurately
+# https://schedule.readthedocs.io/en/stable/
 
-    # subprocess.run("python3 ETHUSDT.py & python3 BTCUSDT.py & python3 BNBUSDT.py & " + 
+def entry():
+
+ # subprocess.run("python3 ETHUSDT.py & python3 BTCUSDT.py & python3 BNBUSDT.py & " + 
     #     "python3 BCHUSDT.py & python3 XRPUSDT.py & python3 EOSUSDT.py & python3 LTCUSDT.py & python3 TRXUSDT.py", shell=True)
 
     subprocess.run("python3 BTCUSDT-JL.py", shell=True)
-
-# https://stackoverflow.com/questions/1724693/find-a-file-in-python
-# import os
-
-# def find(name, path):
-#     for root, dirs, files in os.walk(path):
-#         if name in files:
-#             return os.path.join(root, name)
+    return
 
 #=====================================================================================================================
 
-if __name__ == '__main__':
+client = Client(config.BINANCE_API_KEY,config.BINANCE_SECRET_KEY)
+info = client.get_server_time()
+ts = str(info["serverTime"])
+t1 = ts[:-3]
+t2 = int(t1)
+server_time = datetime.fromtimestamp(t2).strftime('%Y-%m-%d %H:%M:%S')
+datetime_object = datetime.strptime(server_time, '%Y-%m-%d %H:%M:%S')
+print(datetime_object)
+hour = datetime_object.strftime("%H")
+minute = int(datetime_object.strftime("%M"))
+second = int(datetime_object.strftime("%S"))  
+second += 2
 
-    client = Client(config.BINANCE_API_KEY,config.BINANCE_SECRET_KEY)
-    info = client.get_server_time()
-    ts = str(info["serverTime"])
-    t1 = ts[:-3]
-    t2 = int(t1)
-    server_time = datetime.fromtimestamp(t2).strftime('%Y-%m-%d %H:%M:%S')
-    datetime_object = datetime.strptime(server_time, '%Y-%m-%d %H:%M:%S')
-    print(datetime_object)
-    hour = datetime_object.strftime("%H")
-    minute = int(datetime_object.strftime("%M"))
-    second = int(datetime_object.strftime("%S"))  
+while True:
 
-    main()
+    second += 1
+
+    if minute == 60 and second == 1:
+        
+        entry()
+        
+    if second == 60:
+        second = 0
+        minute += 1
     
-    while 1 == 1: 
-        # timer = minute + ":" + second
-        # print(minute, ":", second)
-        second += 1
+    if minute == 60:
+        minute = 0
+    
+    time.sleep(1)
+    print(second)
 
-        # main()
+#=====================================================================================================================
+
+# schedule.every(10).minutes.do(entry)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+# schedule.every().minute.at(":17").do(job)
+
+        # result = db.get_status()
+        # print(result)
         # quit()
+        # yy = 0
+        # for y in result:
+        #     yy += 1
 
-        #ENTRY
-        # if int(repr(minute)[-1]) == 5 and second == 1: #5m
-        #     print("ENTRY 5m")
-        #     print(minute, ":", second)
-        #     main()
-            #detect toggled at 5m
+        # if yy > 0:
 
-        # if int(repr(minute)[-1]) == 0 and second == 1: #5m
-        #     print("ENTRY 5m")
-        #     print(minute, ":", second)
+        #     xx = 0
+        #     for x in result:
+        #         xx += 1
+        #         timeframe = x['timeframe']
+        #         order_type = x['order_type']
+        #         entry_date = x['entry_date']
+        #         pair = x['pair']
+        #         tf = int(timeframe[:-1])
 
-        # if minute == 15 and second == 1: #15m
-        #     print("ENTRY 15m")
-        #     print(minute, ":", second)
-
-        # if minute == 30 and second == 1: #15m/30m
-        #     print("ENTRY 15m/30m")
-        #     print(minute, ":", second)
-
-        # if minute == 45 and second == 1: #15m
-        #     print("ENTRY 15m")
-        #     print(minute, ":", second)
-
-        # if minute == 0 and second == 1: #15m/#30m/1h
-        #     print("ENTRY 15m/#30m/1h")
-        #     print(minute, ":", second)
-        #     main()
-
-        #=================
-        #EXIT Store Close price
-        # if int(repr(minute)[-1]) == 4 and second == 55: #5m
-        #     print("EXIT 5m")
-        #     print(minute, ":", second)
-        #     main()
-
-        # if int(repr(minute)[-1]) == 9 and second == 59: #5m
-        #     print("EXIT 5m")
-        #     print(minute, ":", second)
-
-        # if minute == 14 and second == 55: #15m
-        #     print("EXIT 15m")
-        #     print(minute, ":", second)
-        #     main()
-
-        # if minute == 29 and second == 59: #15m/30m
-        #     print("EXIT 15m/30m")
-        #     print(minute, ":", second)
-
-        # if minute == 44 and second == 59: #15m
-        #     print("EXIT 15m")
-        #     print(minute, ":", second)
-
-        if minute == 59 and second == 59: #15m/#30m/1h
-            print("EXIT 15m/#30m/1h")
-            print(minute, ":", second)
-            main()
-
-        if second == 60:
-            second = 0
-            minute += 1
-        
-        if minute == 60:
-            minute = 0
-
-        info = client.get_server_time()
-        ts = str(info["serverTime"])
-        t1 = ts[:-3]
-        t2 = int(t1)
-        server_time = datetime.fromtimestamp(t2).strftime('%Y-%m-%d %H:%M:%S')
-        datetime_object = datetime.strptime(server_time, '%Y-%m-%d %H:%M:%S')
-        # print(datetime_object)
-        # quit()
-        
-        time.sleep(1)
-
-# BTCUSDT
-# ETHUSDT
-# BNBUSDT
-# XRPUSDT
-# SOLUSDT
-# LUNAUSDT
-# ADAUSDT
-# USTUSDT
-# BUSDUSDT
-# DOGEUSDT
-# AVAXUSDT
-# DOTUSDT
-# SHIBUSDT
-# WBTCUSDT
-# DAIUSDT
-# MATICUSDT
+            # print(timeframe, entry_date, pair, tf, datetime_object)
+            # print(type(entry_date))
+            # db.put_orderTest_Exit(pair, order_type, timeframe, win_lose)  

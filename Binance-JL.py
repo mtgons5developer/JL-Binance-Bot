9a62368a93095a096be54b396752166c7acb7aea
Ernesto Almeda <mtgons5.ea@gmail.com>
2022-06-06 15:29:33 +0800

    subprocess

--- Binance-JL.py
+++ Binance-JL.py
@@ -1,6 +1,4 @@
-from datetime import datetime
-from datetime import timedelta
-
+from datetime import datetime, timedelta
 import time
 import subprocess
 
@@ -14,14 +12,8 @@ import config
 # https://stackoverflow.com/questions/22715086/scheduling-python-script-to-run-every-hour-accurately
 # https://schedule.readthedocs.io/en/stable/
 
-def entry():
-
- # subprocess.run("python3 ETHUSDT.py & python3 BTCUSDT.py & python3 BNBUSDT.py & " + 
-    #     "python3 BCHUSDT.py & python3 XRPUSDT.py & python3 EOSUSDT.py & python3 LTCUSDT.py & python3 TRXUSDT.py", shell=True)
-
-    # subprocess.run("python3 BTCUSDT-JL.py", shell=True)
-    subprocess.run("python3 BTCUSDT-JL.py & python3 ETHUSDT.py", shell=True)
-    return
+# BTCUSDT, ETHUSDT, BNBUSDT, XRPUSDT, SOLUSDT, LUNAUSDT, ADAUSDT, USTUSDT, BUSDUSDT, 
+# DOGEUSDT, AVAXUSDT, DOTUSDT, SHIBUSDT, WBTCUSDT, DAIUSDT, MATICUSDT
 
 #=====================================================================================================================
 
@@ -41,6 +33,41 @@ def timer():
     second = int(datetime_object.strftime("%S"))
     second += 2
 
+def entry():
+    
+    BTCUSDT = ""
+    ETHUSDT = ""
+    BNBUSDT = ""
+    XRPUSDT = ""
+    SOLUSDT = ""
+    ADAUSDT = ""
+
+    result = db.get_toggle()
+    yy = 0
+    for y in result:
+        yy += 1
+
+    xx = 0
+    for x in result:
+        xx += 1
+        pair = x['pair']
+        print(pair)
+
+        if pair == "BTCUSDT":
+            BTCUSDT = "python3 BTCUSDT-JL.py"
+        elif pair == "ETHUSDT":
+            ETHUSDT = "& python3 ETHUSDT.py"
+        elif pair == "BNBUSDT":
+            BNBUSDT = "& python3 BNBUSDT.py"
+        elif pair == "XRPUSDT": 
+            XRPUSDT = "& python3 XRPUSDT.py"
+        elif pair == "SOLUSDT":
+            SOLUSDT = "& python3 SOLUSDT.py"
+        elif pair == "ADAUSDT":
+            ADAUSDT = "& python3 ADAUSDT.py"
+
+    subprocess.run(BTCUSDT + ETHUSDT + BNBUSDT + XRPUSDT + SOLUSDT + ADAUSDT, shell=True)
+
 result = db.get_toggle()
 yy = 0
 for y in result:
@@ -50,7 +77,9 @@ xx = 0
 for x in result:
     xx += 1
     pair = x['pair']
-    print("Active Pairs:", pair)
+    tf = x['timeframe']
+    db.put_order_Exit(pair)
+    print("Active Pairs:", pair, tf)
 
 timer()
 
@@ -58,30 +87,28 @@ while True:
 
     #if coin is activated then read order entry at X time.
     second += 1
-    # print(minute, second)
-
-    if second == 60:
-        second = 0
-        minute += 1
+    print(minute, second)
     
     if second == 1:
-        if minute == 5 or minute == 10 or minute == 15 or minute == 20 or minute == 25 or minute == 30 or minute == 35 or minute == 40 or minute == 45 or minute == 50 or minute == 55 or minute == 0:
-        # if minute == 26 and second == 1:
+        if minute == 28:# or minute == 10 or minute == 15 or minute == 20 or minute == 25 or minute == 30 or minute == 35 or minute == 40 or minute == 45 or minute == 50 or minute == 55 or minute == 0:
             print("Entry", minute)
             entry()
             timer()
             print("Exit")
 
-    # if minute == 26 and second == 1:
-        # print("Entry")
-        # entry()
-        # timer()
-        # print("Exit")
+    # if minute == 4 and second == 1:
+    #     print("Entry")
+    #     entry()
+    #     timer()
+    #     print("Exit")
 
-    if minute == 60:
+    if second >= 60:
+        second = 0
+        minute += 1
+
+    if minute >= 60:
         minute = 0
     
     time.sleep(1)
-
 
 
